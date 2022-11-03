26325093df9d9b2fbaf8c6e47d613d6a838abbbd
lscheinhorn <lukescheinhorn@gmail.com>
2022-11-03 14:32:13 -0400

    11/3/2022

---
+++ src/Components/CartDrawer/CartDrawer.js
@@ -0,0 +1,65 @@
+import './CartDrawer.css'
+
+export default function CartDrawer () {
+    let drawer = null;
+            
+    function init() {
+      drawer = document.getElementById('cart_drawer');
+      drawer.style.position= 'relative'; 
+      drawer.style.left = '0px'; 
+      drawer.style.top = '0px'; 
+    
+    }
+    const open = () => {
+      
+            
+      const distance = 1
+      console.log(distance)
+      const hop = () => {
+        drawer.style.left = parseFloat(drawer.style.left) + distance + '%'
+      } 
+      
+      const open = (frames, time) => {
+        frames--
+        if(frames){
+          setTimeout(() => {
+            hop()
+            smoothHop(frames, time)
+          }, time)
+        }
+      }
+      open(75, 4)
+    }
+    
+    function close(){
+      drawer.style.left = '0px';
+    }
+    
+    window.onload = init;
+
+
+   const slideDrawer = () => {
+       const drawer = document.getElementById("cart_drawer")
+       const open = () => {
+           drawer.style.left 
+       }
+   }
+   
+    return (
+        <div id="cart_sleave">
+            <div id="cart_drawer">
+                <button id="drawer_tab" ><</button>
+                <div>
+                    Div 1
+                </div>
+                <div>
+                    Div 2
+                </div>
+                <div>
+                    Div 3
+                </div>
+            </div>
+        </div>
+        
+    )
+}
