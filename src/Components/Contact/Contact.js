bcf7e63204654e6c2a94345c888fea91edd8cd4e
lscheinhorn <lukescheinhorn@gmail.com>
2022-12-13 22:08:33 -0500

    fix: contact form reset

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -5,7 +5,6 @@ export default function Contact() {
     const form = useRef();
  
     const sendEmail = (e) => {
-        e.preventDefault(); // prevents the page from reloading when you hit “Send”
         emailjs.sendForm('service_6n5ow3f', 'template_9y2c7vf', form.current, 'anRu1WXRFLGM58t0y')
         .then((result) => {
          // show the user a success message
@@ -17,11 +16,13 @@ export default function Contact() {
     return (
         <form ref={form} onSubmit={sendEmail}>
             <div className="form-group">
-            <label for="name">Name</label>
-            <input className="form-control" type="text" name="from_name" />
+                <label for="name">Name</label>
+                <input className="form-control" type="text" name="from_name" />
                 <label for="exampleInputEmail1">Email address</label>
                 <input className="form-control" type="email" name="user_email" />
                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.<br></br></small>
+                <label for="phone">Phone</label>
+                <input className="form-control" type="text" name="phone" />
                 <label>Message</label>
                 <textarea className="form-control" name="message" />
                 <input className="btn btn-primary" type="submit" value="Send" />
