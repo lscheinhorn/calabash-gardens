ee23727303b0b50847e124c942206d2bf1505b6d
lscheinhorn <lukescheinhorn@gmail.com>
2022-12-15 18:41:22 -0500

    style: footer logo removal

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,11 +6,11 @@ export default function Footer () {
     return (
         <div className='footer footer_color'>
                 <div className='title_container footer_color' >
-                    <div className='purple_logo footer_color'>
+                    {/*<div className='purple_logo footer_color'>
                         <img 
                             alt='purple flower logo' 
                             src='https://static.wixstatic.com/media/a339f1_08e8e62aa51141dca62b8ae7529790e1~mv2.png/v1/fill/w_146,h_80,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/purple%20logo%20.png'                         />
-                    </div>
+    </div>*/}
                     <div className='title footer_color'>
                         <h1 className='footer_color'>{ content.home.header.title }</h1>
                         <p className='footer_color'>{ content.home.header.subtitle }</p>
