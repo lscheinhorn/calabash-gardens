d2cb21d45a5775bbb4c863ce288dd1a55827e982
lscheinhorn <lukescheinhorn@gmail.com>
2023-02-07 14:33:11 -0500

    feat: product page styling

--- src/Components/ProductPage/ProductPage.js
+++ src/Components/ProductPage/ProductPage.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'
 import { products } from '../../resources/products'
 import { addCartItem } from '../Cart/cartSlice'
 import { useDispatch } from 'react-redux'
+import {  Link } from 'react-router-dom'
 
 export default function ProductPage () {
     const dispatch = useDispatch()
@@ -32,17 +33,28 @@ export default function ProductPage () {
 
    
 
-    const handleAddToCart = () => {
+    const handleAddCartItem = () => {
         dispatch(addCartItem(product))
     }
 
     return (
-        <div>
-            <h2>{ title }</h2>
+
+        <div className="productPage_container">
             <img src={ featured } alt={ featured } />
+            <h4>{ title }</h4>
             <p>{ info }</p>
             <p>${ price }</p>
-            <button className="add_to_cart" onClick={ handleAddToCart } >Add To Cart</button>
+            <button className="add_to_cart btn btn-outline-primary" onClick={ handleAddCartItem } >Add To Cart</button>
+            <Link id="proguctPage-continue-shopping" className="continue-shopping  btn btn-secondary" to="../calabash-gardens/shop">Continue Shopping</Link>
+
         </div>
+
+        // <div>
+        //     <h2>{ title }</h2>
+        //     <img src={ featured } alt={ featured } />
+        //     <p>{ info }</p>
+        //     <p>${ price }</p>
+        //     <button className="add_to_cart" onClick={ handleAddToCart } >Add To Cart</button>
+        // </div>
     )
 }
