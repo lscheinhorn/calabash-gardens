3521eb8de93d1b113d6dab0f62cd40bec73c509f
lscheinhorn <lukescheinhorn@gmail.com>
2023-02-07 18:36:41 -0500

    feat: banner parallax mobile

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -2,8 +2,18 @@ import './Banner.css'
 import { content } from '../../resources/content'
 
 export default function Banner () {
+
+    document.querySelector("body").onscroll = function slowScroll() {  
+        let scrolltotop = document.scrollingElement.scrollTop
+        const target = document.getElementById("banner")
+        const xvalue = "center"
+        const factor = .05;
+        let yvalue = scrolltotop * factor - 400
+        target.style.backgroundPosition = xvalue + " " + yvalue +"px"
+      }
+
     return (
-        <div className='banner'>
+        <div id='banner'>
             <h1>{ content.home.banner.title }</h1>
             <h4>{ content.home.banner.subtitle_1 }<br>
                 </br>{ content.home.banner.subtitle_2 }
