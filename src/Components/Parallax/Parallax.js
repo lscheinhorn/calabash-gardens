ad60cad56b5f7e3b7717af2681868d53c5db12a0
lscheinhorn <lukescheinhorn@gmail.com>
2023-02-07 18:25:46 -0500

    chore: parallax fix

--- src/Components/Parallax/Parallax.js
+++ src/Components/Parallax/Parallax.js
@@ -6,8 +6,8 @@ document.querySelector("body").onscroll = function slowScroll() {
     let scrolltotop = document.scrollingElement.scrollTop
     const target = document.getElementById("parallax_container")
     const xvalue = "center"
-    const factor = -0.6;
-    let yvalue = scrolltotop * factor + 1100
+    const factor = .05;
+    let yvalue = scrolltotop * factor - 200
     target.style.backgroundPosition = xvalue + " " + yvalue +"px"
   }
 
