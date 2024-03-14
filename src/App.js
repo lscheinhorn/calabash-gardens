7d7bb9df06e419af6d65bf6327aefd1d9a68213e
lscheinhorn <lukescheinhorn@gmail.com>
2024-03-14 19:11:41 -0400

    feat: shop product fit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,48 @@
-import './App.css'
-import Header from './Components/Header/Header'
-import Main from './Components/Main/Main'
-import Shop from './Components/Shop/Shop.js'
-import Footer from './Components/Footer/Footer'
-import ProductPage from './Components/ProductPage/ProductPage'
-import Cart from './Components/Cart/Cart'
-import Contact from './Components/Contact/Contact'
-import Admin from './Components/Admin/Admin'
+import './App.css';
+import Header from './Components/Header/Header';
+import Main from './Components/Main/Main';
+import Shop from './Components/Shop/Shop.js';
+import Footer from './Components/Footer/Footer';
+import ProductPage from './Components/ProductPage/ProductPage';
+import Cart from './Components/Cart/Cart';
+import Contact from './Components/Contact/Contact';
+import Admin from './Components/Admin/Admin';
 
 
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
-// alert("This site is currently under construction. Please note that in order to purchase saffron you must contact Calabash Gardens directly through the contact form or email us at calabashgardens@gmail.com")
+// Alert message - This site is currently under construction. Please note that in order to purchase saffron you must contact Calabash Gardens directly through the contact form or email us at calabashgardens@gmail.com
+// alert("This site is currently under construction. Please note that in order to purchase saffron you must contact Calabash Gardens directly through the contact form or email us at calabashgardens@gmail.com");
 
 function App() {
   return (
-  <div className="App app_wrap">
+    <div className="App app_wrap">
 
-    <HashRouter>
-      <Header />
-      <Routes>
-        <Route 
-          path="/products/:key" 
-          element={<ProductPage />} 
-        />
-        <Route 
-          path="/admin" 
-          element={<Admin />} 
-        />
-        <Route 
-          path="/shop" 
-          element={<Shop />} 
-        />
-        <Route 
-          path="/cart"
-          element={<Cart />}
-        />
-        <Route 
-          path="/contact"
-          element={<Contact />}
-        />
-        <Route 
-          path="/" 
-          element={<Main />} 
-        />
-      
-      </Routes>
-      <Footer />
-    </HashRouter>
-  </div>
+      <HashRouter>
+        <Header />
+        <Routes>
+          {/* Route for displaying individual product page */}
+          <Route path="/products/:key" element={<ProductPage />} />
 
-  )
+          {/* Route for admin panel */}
+          <Route path="/admin" element={<Admin />} />
+
+          {/* Route for main shop page */}
+          <Route path="/shop" element={<Shop />} />
+
+          {/* Route for shopping cart */}
+          <Route path="/cart" element={<Cart />} />
+
+          {/* Route for contact page */}
+          <Route path="/contact" element={<Contact />} />
+
+          {/* Default route for home/main page */}
+          <Route path="/" element={<Main />} />
+        </Routes>
+        <Footer />
+      </HashRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
