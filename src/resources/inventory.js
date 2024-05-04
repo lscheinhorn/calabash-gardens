40d38ed0c464e19718eb4a339308eaf81191ac4d
lscheinhorn <lukescheinhorn@gmail.com>
2024-05-04 12:29:32 -0400

    Update: Inventory set to 30 for events

--- src/resources/inventory.js
+++ src/resources/inventory.js
@@ -4,22 +4,22 @@ export const eventsInventory = {
         stock: 0
     },
     'Ma-Der! Ma-Der! \nLao Cuisine By Chef Mary and Phet July 5th 2024' : {
-        stock: 10
+        stock: 30
     },
     'Ma-Der! Ma-Der! \nLao Cuisine By Chef Mary and Phet July 6th 2024' : {
-        stock: 11
+        stock: 30
     },
     'Fleur De Mar \nby Chef Cole Conrad Cohen August 2nd 2023' : {
-        stock: 12
+        stock: 30
     },
     'Fleur De Mar \nby Chef Cole Conrad Cohen August 3rd 2023' : {
-        stock: 13
+        stock: 30
     },
     'Chuseok \nby Mountain Song Kitchen September 5th 2024' : {
-        stock: 14
+        stock: 30
     },
     'Chuseok \nby Mountain Song Kitchen September 6th 2024' : {
-        stock: 15
+        stock: 30
     },
 }
  
