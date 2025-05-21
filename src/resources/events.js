bb572019afe219e1ea567adb2580884f4f2fc7e8
lscheinhorn <lukescheinhorn@gmail.com>
2025-05-21 17:45:20 -0400

    Update: new events 2025

--- src/resources/events.js
+++ src/resources/events.js
@@ -36,18 +36,19 @@ export const createKey = (input) => {
 
 
 export const experienceBlurb = [
-  `The Calabash Experience is our way to bring the finest quality Calabash saffron, wild foraged foods, organic produce and regeneratively grown meats from hyper local farms to your plate in the most beautiful ways. We are harboring a space for creativity, diversity and culture with an air of accessibility, it’s easier than you may think to eat local and eat well. With Jette’s two decade run in the service industry it only felt right to begin hosting Elevated Farm to Plate Experiences on the lush lawn of our farm house, serving up the finest, healthiest and most local ingredients possible all with a saffron forward mindset. We are purveyors of fine foods and spirits, of a regenerative and responsible nature, with a flare for the diverse and unexpected. We feature as many Chefs as we can, highlighting their unique cultural heritage and allowing them total creativity in what they bring to the table. Our two requirements are that the menus they create are saffron forward, inviting our guests to enjoy a unique experience duplicated nowhere else, and all aspects be made in house with only the finest quality local ingredients. Using wildcrafted, organic and regeneratively  grown meats and produce allows us to present a completely sustainable experience while also maintaining a  superior level of health for guests and the planet alike. We are merging accessibility, diversity, uniqueness, health and above all flavor, for an incredible evening of food, spirits, and of course most of all enjoyment!`,
-  `There are 30 seats available for each seating of the Calabash Experiences in 2024. We can accommodate Vegetarians and Gluten Free for most of our meals, please hit the Vegetarian or Gluten Free options when purchasing tickets. There is an additional $10 fee for either or both options. We may not be able to do Gluten Free or Vegetarian for every Experience, please check the descriptions. If you are purchasing a mix of meat, veggie or gluten free please make sure you have picked the correct option before clicking buy tickets, and check again once in the cart.`,
-`Events are held at 1831 Fish Pond Rd Wells River Vermont 05081. Arrival time is 5pm with seating at 5:30pm. Menus are subject to change depending on local availability and seasonality.`,
-  `The Calabash Gardens team works incredibly hard to bring these events to you, in light of all the hard work we do, and the associated cost of these events, 50% of the ticket price is charged at time of purchase with no refund available. The second 50% is taken at time of arrival as you check in. Thank you for understanding. Gratuities are appreciated for our incredibly hard working staff, tips are split evenly between all function employees.`
-	
+    `The Calabash Experience is our way to bring the finest quality Calabash saffron, wild foraged foods, organic produce and regeneratively grown meats from hyper local farms to your plate in the most beautiful ways. We are harboring a space for creativity, diversity and culture with an air of accessibility, it’s easier than you may think to eat local, eat well and use saffron! With Jette’s two decade run in the service industry it only felt right to begin hosting Elevated Farm to Plate Experiences on the lush lawn of our farm house, serving up the finest, healthiest and most local ingredients possible all with a saffron forward mindset. We are purveyors of fine foods and spirits, of a regenerative and responsible nature, with a flare for the diverse and unexpected. We have an incredible core team and bring in new chefs, highlighting their unique cultural heritage while allowing them total creativity in what they create for your plate. Our two requirements are that the menus they create are saffron forward, inviting our guests to enjoy a unique experience duplicated nowhere else, and all aspects be made in house with only the finest quality local ingredients. Using wildcrafted, organic and regeneratively  grown meats and produce allows us to present a completely sustainable experience while also maintaining a  superior level of health for guests and the planet alike. We are merging accessibility, diversity, uniqueness, health and above all flavor, for an incredible evening of food, spirits, and of course most of all enjoyment!`,
+    `The fine dining experiences take a 50% non refundable deposit, the second 50% is taken at time of service. The music experiences are 100% at time of purchase non refundable.`,
+    `There are 30 seats available for each fine dining Calabash Experience and unlimited for music events. We can accommodate Vegetarians and Gluten Free for most of our meals, please hit the Vegetarian or Gluten Free options when purchasing tickets. There is an additional $10 fee for either or both options. We may not be able to do Gluten Free or Vegetarian for every Experience, please check the descriptions. If you are purchasing a mix of meat, veggie or gluten free please make sure you have picked the correct option before clicking buy tickets, and check again once in the cart.`,
+    `Events are held at 1831 Fish Pond Rd Wells River Vermont 05081. Arrival time is 5pm with seating at 5:30pm. Menus are subject to change depending on local availability and seasonality.`,
+    `The Calabash Gardens team works incredibly hard to bring these events to you, in light of all the hard work we do, and the associated cost of these events, 50% of the ticket price is charged at time of purchase with no refund available. The second 50% is taken at time of arrival as you check in. Thank you for understanding. Gratuities are appreciated for our incredibly hard working staff, tips are split evenly between all function employees.`
 ]
 
 
 
 
 export const events = [
-  {
+  
+    {
      title: 'Calabash Experience, Chef Mike Clancy',
      category: "Experience",
      info: [
@@ -75,21 +76,22 @@ export const events = [
       title: `Ma-Der! Ma-Der! \nLao Cuisine By Chef Mary and Phet`,
       category: "Experience",
       info: [
-          `July 5th Calabash Will be hosting a unique Lao experience. Chefs Phet and Mary will be using saffron in a coursed out traditional Lao meal featuring incredible local ingredients and cocktail or mocktail pairings. The Experience can accommodate both Vegetarian and Gluten free. A 50% non refundable deposit of $55 to book your seats, the second $55 is payed at time of event.`,
+          `July 5th and 6th Calabash Will be hosting a unique Lao experience. Chefs Phet and Mary will be using saffron in a coursed out traditional Lao meal featuring incredible local ingredients and cocktail or mocktail pairings. The Experience can accommodate both Vegetarian and Gluten free. A 50% non refundable deposit of $55 to book your seats, the second $55 is payed at time of event.`,
           `Meet your Chefs!`,
           `Phet Keomanyvahn grew up in the Northeast Kingdom and graduated from St. Johnsbury Academy. She went on to obtain her B.A. in Anthropology at The University of North Carolina-Greensboro and then her M.A. in Public Administration at the University of Vermont. She currently works for the City and Burlington as Program Manager in the Racial Equity, Inclusion and Belonging Office to further advance the City’s diversity and equity goals. Phet enjoys spending her time with family and friends, hiking, practicing yoga, and foraging with her dog Moo `,
           `Mary Nasouluk-G works in finance and received her bachelor’s degree from the University of North Carolina in Accounting. Born and raised in Boston to Lao immigrants, a sense of community was built around cooking and sharing Laos food. Her family prepared staple Laos dishes for gatherings and local community events. She continues this tradition with her two sons by sharing their culture through cooking.`,
        ],
        // Date must be as follows: Date(year, month, day) format with month starting at index 0 for January
-      date: new Date(2024, 5, 5),
+      date: new Date(2024, 6, 5),
       eventDates: [
-          "",
+          "July 5th 2024",
+          "July 6th 2024",
       ],
       link: require("../resources/images/Ma-Der_Ma-Der_Menu.pdf"),
       priceOptions: ['55.00'],
       shipping: '0.00',
       isActive: true,
-      inStock: false,
+      inStock: true,
       photos: [
           require('../resources/images/Moo_and_Mom_Fall_Joy_.jpg'),
           require('../resources/images/Mary_and_boys.jpg'),
@@ -136,7 +138,7 @@ export const events = [
       title: 'Chuseok \nby Mountain Song Kitchen',
       category: "Experience",
       info: [
-          `For one night only we have Mountain Song Kitchen from Newbury, VT hosting this experience, welcome to Chuseok! A traditionally based Korean meal fussed with a saffron forward theme, an experience never before tasted! Book this experience today for a first in a lifetime flavor mash up. This Experience can accommodate Vegetarian but not gluten free. A 50% non refundable deposit to book your seats, the second half is payed at time of event.`,          
+          `September 6th for one night only we have Mountain Song Kitchen from Newbury, VT hosting this experience, welcome to Chuseok! A traditionally based Korean meal fussed with a saffron forward theme, an experience never before tasted! Book this experience today for a first in a lifetime flavor mash up. This Experience can accommodate Vegetarian but not gluten free. A 50% non refundable deposit to book your seats, the second half is payed at time of event.`,          
     `Meet your Chef!`,
           `My Mother is a chef and in our household sharing food with friends and family was an integral part of the day. The importance of food, the time spent preparing it and the time we shared enjoying it was instilled in me at a young age. As a teenager I started working in the food industry and over the course of the next ten years I worked up from dishwasher to chef. Seven years ago I decided to take a break from working in kitchens and reevaluate, and reassess my relationship with the industry and with Korean food in particular. I was curious about how I could honor the traditions of Korean cuisine while also accommodating the western palette. Most importantly, having started this business with Jesa, I've been having a pretty good time. -Driscoll`,
           `Moving to Vermont and starting a garden has helped my husband and I tie our cuisine to the food source. We began using korean natural farming and organic methods in our garden. The focus of our garden inputs are sourced through foraging and using food waste to cultivate specific fertilizers, indigenous microorganisms, healthy bacteria and fungi for soil health. The garden feeds the kitchen, feeds the garden, feeds the kitchen. -Jesa`,  
@@ -146,15 +148,15 @@ export const events = [
 
 
       // Date must be as follows: Date(year, month, day) format with month starting at index 0 for January
-      date: new Date(2024, 7, 6),
+      date: new Date(2024, 8, 6),
       eventDates: [
-          
+          "September 6th 2024",
       ],
       link: require("../resources/images/Mountain_Song_Menu.png"),
       priceOptions: ['55.00'],
       shipping: '0.00',
       isActive: true,
-      inStock: false,
+      inStock: true,
       photos: [
           require('../resources/images/Driscoll_and_Jesa.jpg'),
       ],
@@ -166,7 +168,7 @@ export const events = [
       title: 'A Taste of Season in Vermont \nby Chefs Samantha Langevin and Jeannie Kovacs',
       category: "Experience",
       info: [
-       ` October 5th Samantha Langevin and Jeannie Kovacs are bringing to Calabash a power packed menu full of unique mediterranean themed vermont grown courses paired with cocktails to spotlight the early fall colors and flavors. Come welcome autumn with the warm flavor of saffron and the gifts of the land!.`,   
+       ` October 5th and 6th Samantha Langevin and Jeannie Kovacs are bringing to Calabash a power packed menu full of unique mediterranean themed vermont grown courses paired with cocktails to spotlight the early fall colors and flavors. Come welcome autumn with the warm flavor of saffron and the gifts of the land!.`,   
 `Samantha Langevin and Jeannie Kovacs are a daughter & mother who have spent more time eating together than anything else they can think of. Jeannie raised Samantha and her siblings in Vermont and Texas in a household that required adventurous eaters, with Samantha’s formative food memories including coquilles St Jacques, homemade tacos, pan fried trout, and the best birthday cakes.
 Samantha has worked as a chef in farm to table dining for over 10 years, getting her start in the Bay Area of California before returning to Vermont. Most recently the Chef de Cuisine at American Flatbread, Middlebury Hearth, she now works for The Vermont Releaf Collective, helping in its mission to amplify the voices of BIPOC in Land, Environment, Agriculture, & Food. She lives on a small farm with her partner and will enthusiastically discuss Vermont cheese with
 anyone who cares to listen.`,
@@ -177,23 +179,120 @@ anyone who cares to listen.`,
 
 
       // Date must be as follows: Date(year, month, day) format with month starting at index 0 for January
-      date: new Date(2024, 9, 5),
+      date: new Date(2024, 9, 4),
       eventDates: [
-          "October 5th 2024",
+          "October 4th 2024",
+    "October 5th 2024",
       ],
-      link: require("../resources/images/a_taste_of_season.pdf"),
+      link: require("../resources/images/Mountain_Song_Menu.png"),
       priceOptions: ['55.00'],
       shipping: '0.00',
       isActive: true,
-      inStock: false,
+      inStock: true,
       photos: [
-          require('../resources/images/samantha_and_jeannie.jpg'),
+          require('../resources/images/Driscoll_and_Jesa.jpg'),
       ],
-      key: `A Taste of Season in Vermont \nby Chefs Samantha Langevin and Jeannie Kovacs October 5th 2024`
-    //   get key() {
-    //       return createKey(this.title)
-    //   }
+      get key() {
+          return createKey(this.title)
+      }
    },
+
+   {
+    title: 'Calabash Experience, Solstice Dance Party, Saffron Bites',
+     category: "Experience",
+     info: [
+         `June 21st 2025 we are hosting the Young Brothers Band for a rocking dance party with saffron forward passed hor d'oeuvres and a bonfire if the weather permits. This band brings the deep rhythms of the blues and r&b. Get ready for a real dance party treat!`,
+         `Local spirits with herbal infusions, beer, wine and mocktails will be served at a CASH bar, we can run credit for this event if you want to open a tab.  All produce and meats featured are from some of our favorite local farms. Our chefs for this event are Geoffrey Turcich and Cole Conrad Cohen.  `
+     ],
+     // Date must be as follows: Date(year, month, day) format with month starting at index 0 for January
+     date: new Date(2025, 5, 21),
+     eventDates: [
+          "June 21st, 2025",
+      ],
+      link: require("../resources/Menu.docx.pdf"),
+      priceOptions: ['45.00'],
+      shipping: '0.00',
+      isActive: true,
+      inStock: true,
+      photos:[
+
+      ],
+      get key() {
+         return createKey(this.title)
+      }
+    },
+    {
+        title: `Calabash Experience, Into the Wolf's Mouth`,
+        category: "Experience",
+     info: [
+         `July 26th, 2025 Calabash Gardens and Into the Wolf’s Mouth is hosting a farm to table fine dining experience for your culinary pleasure. Bringing a Burlington staple to the Upper Valley`,
+         `Local spirits with herbal infusion pairings or mocktails if preferred are included in this event.  All produce and meats featured are from some of our favorite local farms and Into the Wolf’s Mouth has full culinary creative license. Saffron from Calabash Gardens featured in each course 30 seats only, 50% non refundable deposit to secure your spot, 50% taken upon arrival at time of service. `
+     ],
+     // Date must be as follows: Date(year, month, day) format with month starting at index 0 for January
+     date: new Date(2025, 6, 26),
+     eventDates: [
+          "July 26th, 2025",
+      ],
+      link: require("../resources/Menu.docx.pdf"),
+      priceOptions: ['60.00'],
+      shipping: '0.00',
+      isActive: true,
+      inStock: true,
+      photos:[
+
+      ],
+      get key() {
+         return createKey(this.title)
+      }
+    },
+    {
+      title: 'Calabash Experience, Deep Summer Dance Party, Saffron Bites',
+     category: "Experience",
+     info: [
+         `August 23rd 2025 Calabash Gardens welcomes the hosting the Pilgrims for a rocking dance party with saffron forward passed hor d'oeuvres. The Pilgrims are a rock band reminiscent of the 60’s but bringing all the influences from all the decades. `,
+         `Local spirits with herbal infusions, beer, wine and mocktails will be served at a CASH bar, we can run credit for this event if you want to open a tab.  All produce and meats featured are from some of our favorite local farms. Our chefs for this event are Geoffrey Turcich and Cole Conrad Cohen.  `
+     ],
+     // Date must be as follows: Date(year, month, day) format with month starting at index 0 for January
+     date: new Date(2025, 7, 23),
+     eventDates: [
+          "August 23rd, 2025",
+      ],
+      link: require("../resources/Menu.docx.pdf"),
+      priceOptions: ['45.00'],
+      shipping: '0.00',
+      isActive: true,
+      inStock: true,
+      photos:[
+
+      ],
+      get key() {
+         return createKey(this.title)
+      }
+    },
+    {
+        title: 'Calabash Experience, Chad Lumbra',
+     category: "Experience",
+     info: [
+         `September 20th, 2025 Calabash Gardens and Chef Chad Lumbra are hosting a farm to table fine dining experience for your culinary pleasure.`,
+         `Local spirits with herbal infusion pairings or mocktails if preferred are included in this event.  All produce and meats featured are from some of our favorite local farms and Cgad takes full culinary creative license. Saffron from Calabash Gardens featured in each course 30 seats only, 50% non refundable deposit to secure your spot, 50% taken upon arrival at time of service. `
+     ],
+     // Date must be as follows: Date(year, month, day) format with month starting at index 0 for January
+     date: new Date(2025, 8, 20),
+     eventDates: [
+          "September 20th, 2025",
+      ],
+      link: require("../resources/Menu.docx.pdf"),
+      priceOptions: ['60.00'],
+      shipping: '0.00',
+      isActive: true,
+      inStock: true,
+      photos:[
+
+      ],
+      get key() {
+         return createKey(this.title)
+      }
+    },
  
 ]
 
